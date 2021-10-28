c6181685e8118f60e5db42f71acffdf81425876d
Rory Bunker <rorybunker@gmail.com>
2021-10-28 13:33:15 +0900

    Update hausdorff_distance.py

--- hausdorff_distance.py
+++ hausdorff_distance.py
@@ -2,8 +2,8 @@ import numpy as np
 import math
 from hausdorff import hausdorff_distance
 
-def calculate_hausdorff_distance(agent_df):
-  d = hausdorff_distance(np.array(agent_df.loc[play_indx][['x','y']]), 
-                     np.array(agent_df.loc[play_indx][['x','y']]), 
+def calculate_hausdorff_distance(agent_df, play_indx1, play_indx2):
+  d = hausdorff_distance(np.array(agent_df.loc[play_indx1][['x','y']]), 
+                     np.array(agent_df.loc[play_indx2][['x','y']]), 
                      distance='euclidean')
   return d
