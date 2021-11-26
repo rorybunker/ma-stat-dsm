d97e03d7e25998764b638f549930e457c6c6b0cd
Rory Bunker <rorybunker@gmail.com>
2021-11-27 01:16:51 +0900

    Add files via upload

---
+++ plot_traj_len_histograms.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 13:23:50 2021
+
+@author: rorybunker
+"""
+
+   
+traj_num = 0
+after_last_receipt_len = list()
+traj_num = 0
+traj_idx = label_df.index[traj_num][0]
+for traj_num in range(label_df.index[0][0],label_df.index[len(label_df)-1][0]):
+#      after_last_receipt_len.append(len(trajectories[traj_idx][int(t1[traj_idx]):])*0.1)
+    
+# plt.hist(after_last_receipt_len, bins = 20)
+# plt.xlabel("time (s)")
+# plt.ylabel("freq")
+# plt.title("t1: 20-bin histogram")
+
+# traj_num = 0
+# after_last_receipt_len_t2 = list()
+# for traj_num in range(0,len(trajectories)):
+#      after_last_receipt_len_t2.append(len(trajectories[traj_num][int(t2[traj_num]):])*0.1)
+    
+# plt.hist(after_last_receipt_len_t2, bins = 20)
+# plt.xlabel("time (s)")
+# plt.ylabel("freq")
+# plt.title("t2: 20-bin histogram")
