8b370a2a21a1404219241d897b30ce1bb10775e2
Rory Bunker <rorybunker@gmail.com>
2022-06-16 12:07:10 +0900

    Update plot_traj_len_histograms.py

--- ad-hoc/plot_traj_len_histograms.py
+++ ad-hoc/plot_traj_len_histograms.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 13:23:50 2021
+@author: rorybunker
+"""
 
+import matplotlib.pyplot as plt
+plt.hist(ball_df.groupby(ball_df.index).count()['point'], bins = 100)
