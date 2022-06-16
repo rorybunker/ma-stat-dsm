77794c6e087c233be0aa55daa2ed4170f6279a36
Rory Bunker <rorybunker@gmail.com>
2022-06-16 12:27:46 +0900

    Update hausdorff_distance.py

--- archive/hausdorff_distance.py
+++ archive/hausdorff_distance.py
@@ -1,1 +1,13 @@
+import numpy as np
+import math
+from hausdorff import hausdorff_distance
 
+def calculate_hausdorff_distance(agent_df, play_indx1, play_indx2, base_distance):
+  # options for base_distance are 'euclidean', 'manhattan', 'chebyshev', 'cosine'
+  d = hausdorff_distance(np.array(agent_df.loc[play_indx1][['x','y']]), 
+                     np.array(agent_df.loc[play_indx2][['x','y']]), 
+                     distance=base_distance)
+  return d
+
+# example
+# hausdorff_d = calculate_hausdorff_distance(offense_df, 1040, 1042, 'euclidean')
