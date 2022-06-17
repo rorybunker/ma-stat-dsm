bcbb52e034c66773a497c142d9a9f925d0171e4d
Rory Bunker <rorybunker@gmail.com>
2022-06-17 18:37:59 +0900

    Update 1-data-preprocess-ma-stat-dsm.py

--- 1-data-preprocess-ma-stat-dsm.py
+++ 1-data-preprocess-ma-stat-dsm.py
@@ -18,114 +18,87 @@ from shapely.geometry import Point
 import psycopg2
 import sys
 
-# data is in the format [feature, label, index]
-#feature: Position(23:OFxy,DFxy,Ballxyz), player_ID(10), Clock, ShotClock, 
-# Ball_OF, Ball_Hold
-#label: effective attack (1) or ineffective attack (0), T1, T2, score or not
-#index: corresponding to the file at root\nba_attack2\nba_datalength.csv
+# enter your postgres database details in param_dic
+param_dic = {
+    "host"      : "localhost",
+    "database"  : "postgres",
+    "user"      : "postgres",
+    "password"  : "9408"
+}
 
-# enter your working directory under path:
-path = '/Users/rorybunker/dataset_as_a_file_600_games.pkl'
-
-f = open(path, 'rb')
-data = pickle.load(f)
-
-indices = array(data[2].astype(int), dtype=object)
-trajectories = array(data[0], dtype=object)
-label = array(data[1], dtype=object)
-
-label_df = pd.DataFrame(label, index=indices)
-# or if you want to select specific teams e.g. Golden state warriors
-nba_team_id = 1610612744
-label_df = label_df[(label_df[6]==nba_team_id)] 
-# and or fixed number of plays e.g. for testing purposes
-number_of_plays = 100
-label_df = label_df.iloc[0:number_of_plays]
-
-#label data is in the format [label_i,t1,t2,score,shooterID,passerID,team_ID]
-effective = label_df.iloc[:,0]
-t1 = label_df.iloc[:,1]
-t2 = label_df.iloc[:,2]
-t2 = label_df.iloc[:,2][t2>0]
-
-scored = label_df.iloc[:,3]
-scored_df = pd.DataFrame(scored, index=scored.index)
-shooter_id = label_df.iloc[:,4]
-passer_id = label_df.iloc[:,5]
-team_id = label_df.iloc[:,6]
-
-ball_xy = []
-offense_xy_shooter = []
-offense_xy_last_passer = []
-defense_xy_shooter = []
-defense_xy_last_passer = []
-
-play_num = 0
-point_num = 0
-for play_num in range(0,len(t2)):
-        for point_num in range(0,len(trajectories[play_num])):
-            #ball
-            ball_xy.append([t2.index[play_num][0], 
-                            point_num,
-                            (trajectories[play_num][point_num][0]),
-                            (trajectories[play_num][point_num][1])])
-            #shooter
-            offense_xy_shooter.append([t2.index[play_num][0], 
-                                       point_num,
-                            (trajectories[play_num][point_num][2]),
-                            (trajectories[play_num][point_num][3])])
-            #last passer
-            offense_xy_last_passer.append([t2.index[play_num][0], 
+def create_df(trajectories, t_interval):
+    ball_xy = []
+    offense_xy_shooter = []
+    offense_xy_last_passer = []
+    defense_xy_shooter = []
+    defense_xy_last_passer = []
+    
+    play_num = 0
+    point_num = 0
+    for play_num in range(0,len(t_interval)):
+            for point_num in range(0,len(trajectories[play_num])):
+                #ball
+                ball_xy.append([t_interval.index[play_num][0], 
+                                point_num,
+                                (trajectories[play_num][point_num][0]),
+                                (trajectories[play_num][point_num][1])])
+                #shooter
+                offense_xy_shooter.append([t_interval.index[play_num][0], 
                                            point_num,
-                            (trajectories[play_num][point_num][4]),
-                            (trajectories[play_num][point_num][5])])
-            
-            #defender of shooter
-            defense_xy_shooter.append([t2.index[play_num][0], 
-                                       point_num,
-                            (trajectories[play_num][point_num][12]),
-                            (trajectories[play_num][point_num][13])])
-            
-            #defender of last passer
-            defense_xy_last_passer.append([t2.index[play_num][0], 
+                                (trajectories[play_num][point_num][2]),
+                                (trajectories[play_num][point_num][3])])
+                #last passer
+                offense_xy_last_passer.append([t_interval.index[play_num][0], 
+                                               point_num,
+                                (trajectories[play_num][point_num][4]),
+                                (trajectories[play_num][point_num][5])])
+                
+                #defender of shooter
+                defense_xy_shooter.append([t_interval.index[play_num][0], 
                                            point_num,
-                            (trajectories[play_num][point_num][14]),
-                            (trajectories[play_num][point_num][15])])
-            
-            
-ball_df = pd.DataFrame([ball_xy[i][1:4] 
-                        for i in range(0,len(ball_xy))], 
-                       columns=['point','x','y'], 
-                       index=[ball_xy[i][0] for i in range(0,len(ball_xy))])
+                                (trajectories[play_num][point_num][12]),
+                                (trajectories[play_num][point_num][13])])
+                
+                #defender of last passer
+                defense_xy_last_passer.append([t_interval.index[play_num][0], 
+                                               point_num,
+                                (trajectories[play_num][point_num][14]),
+                                (trajectories[play_num][point_num][15])])
+                
+                
+    ball_df = pd.DataFrame([ball_xy[i][1:4] 
+                            for i in range(0,len(ball_xy))], 
+                           columns=['point','x','y'], 
+                           index=[ball_xy[i][0] for i in range(0,len(ball_xy))])
+    
+    offense_df = pd.DataFrame([offense_xy_shooter[i][1:4] 
+                               for i in range(0,len(offense_xy_shooter))], 
+                           columns=['point','x','y'], 
+                           index=[offense_xy_shooter[i][0] 
+                                  for i in range(0,len(offense_xy_shooter))])
+    offense_lp_df = pd.DataFrame([offense_xy_last_passer[i][1:4] 
+                               for i in range(0,len(offense_xy_last_passer))], 
+                           columns=['point','x','y'], 
+                           index=[offense_xy_last_passer[i][0] 
+                                  for i in range(0,len(offense_xy_last_passer))])
+    
+    defense_df = pd.DataFrame([defense_xy_shooter[i][1:4] 
+                               for i in range(0,len(defense_xy_shooter))], 
+                           columns=['point','x','y'], 
+                           index=[defense_xy_shooter[i][0] 
+                                  for i in range(0,len(defense_xy_shooter))])
+    defense_lp_df = pd.DataFrame([defense_xy_last_passer[i][1:4] 
+                               for i in range(0,len(defense_xy_last_passer))], 
+                           columns=['point','x','y'], 
+                           index=[defense_xy_last_passer[i][0] 
+                                  for i in range(0,len(defense_xy_last_passer))])
+    
+    agent_df_list = [ball_df, offense_df, offense_lp_df, defense_df, defense_lp_df]
+    
+    return agent_df_list
 
-offense_df = pd.DataFrame([offense_xy_shooter[i][1:4] 
-                           for i in range(0,len(offense_xy_shooter))], 
-                       columns=['point','x','y'], 
-                       index=[offense_xy_shooter[i][0] 
-                              for i in range(0,len(offense_xy_shooter))])
-offense_lp_df = pd.DataFrame([offense_xy_last_passer[i][1:4] 
-                           for i in range(0,len(offense_xy_last_passer))], 
-                       columns=['point','x','y'], 
-                       index=[offense_xy_last_passer[i][0] 
-                              for i in range(0,len(offense_xy_last_passer))])
-
-defense_df = pd.DataFrame([defense_xy_shooter[i][1:4] 
-                           for i in range(0,len(defense_xy_shooter))], 
-                       columns=['point','x','y'], 
-                       index=[defense_xy_shooter[i][0] 
-                              for i in range(0,len(defense_xy_shooter))])
-defense_lp_df = pd.DataFrame([defense_xy_last_passer[i][1:4] 
-                           for i in range(0,len(defense_xy_last_passer))], 
-                       columns=['point','x','y'], 
-                       index=[defense_xy_last_passer[i][0] 
-                              for i in range(0,len(defense_xy_last_passer))])
-
-
-agent_df_list = [ball_df, offense_df, offense_lp_df, defense_df, defense_lp_df]
-agent_name_list = ["ball", "shooter", "lp", "shooter_defender", "lp_defender"]
-
-def create_point_csv(agent_df_list):
-    with open('point.csv', 'w') as csvfile:
+def create_point_csv(agent_df_list, label):
+    with open('point_ma.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['id', 'aid', 'tid', 'pid', 'label', 'geom'])
         row_num = 0
@@ -145,15 +118,13 @@ def create_point_csv(agent_df_list):
                                              a,
                                              u,
                                              int(df_sub.iloc[p]['point']),
-                                             int(effective[(traj_num,)]),
+                                             int(label[(traj_num,)]),
                                              Point(df_sub.iloc[p]['x'],
                                                    df_sub.iloc[p]['y'])])
                     row_num+=1
 
-create_point_csv(agent_df_list)
-
-def create_trajectory_csv(agent_df_list):
-    with open('trajectory.csv', 'w') as csvfile:
+def create_trajectory_csv(agent_df_list, label):
+    with open('trajectory_ma.csv', 'w') as csvfile:
         writer= csv.writer(csvfile)
         writer.writerow(['id', 'aid', 'tid', 'label','geom'])
         row_num = 0
@@ -165,19 +136,9 @@ def create_trajectory_csv(agent_df_list):
                 traj_num = df_index_unique[u]
                 df_sub = df.loc[traj_num]
                 if len(df_sub) > 0:
-                    writer.writerow([row_num, a, u, int(effective[u]),
+                    writer.writerow([row_num, a, u, int(label[u]),
                                          LineString((df_sub.iloc[:,1:3]).to_numpy())])
                 row_num+=1
-
-create_trajectory_csv(agent_df_list)
-
-# enter your postgres database details in param_dic
-param_dic = {
-    "host"      : "localhost",
-    "database"  : "postgres",
-    "user"      : "postgres",
-    "password"  : "9408"
-}
 
 def connect(params_dic):
     """ Connect to the PostgreSQL database server """
@@ -198,20 +159,84 @@ def delete_table_rows(table_name):
     conn.commit()
     cur.close()
     conn.close()
-
-# delete the table rows that were in the postgres database tables previously
-delete_table_rows('phase_point_ma')
-delete_table_rows('phase_trajectory_ma')
-delete_table_rows('candidates') 
     
-# now, in pgadmin, run these commands to import the csv files generated 
-# into postgres database:
-# COPY phase_point_ma(id, aid, tid, pid, label, geom)
-# FROM '.../point.csv'
-# DELIMITER ',' 
-# CSV HEADER;
+def import_point_ma_table_into_postgres(filename, path):
+    conn = connect(param_dic)
+    cur = conn.cursor()
+    copy_sql = """
+               COPY point_ma(id, aid, tid, pid, label, geom)
+               FROM '/Users/rorybunker/""" + filename + """.csv' 
+               DELIMITER ',' 
+               CSV HEADER;
+               """
+    with open(path, 'r') as f:
+        cur.copy_expert(sql=copy_sql, file=f)
+        conn.commit()
+        cur.close()
 
-# COPY phase_trajectory_ma(id, aid, tid, label, geom)
-# FROM '.../trajectory.csv'
-# DELIMITER ',' 
-# CSV HEADER;
+def import_traj_ma_table_into_postgres(filename, path):
+    conn = connect(param_dic)
+    cur = conn.cursor()
+    copy_sql = """
+               COPY trajectory_ma(id, aid, tid, label, geom)
+               FROM '/Users/rorybunker/""" + filename + """.csv' 
+               DELIMITER ',' 
+               CSV HEADER;
+               """
+    with open(path, 'r') as f:
+        cur.copy_expert(sql=copy_sql, file=f)
+        conn.commit()
+        cur.close()
+ 
+def main():
+    # data is in the format [feature, label, index]
+    #feature: Position(23:OFxy,DFxy,Ballxyz), player_ID(10), Clock, ShotClock, 
+    # Ball_OF, Ball_Hold
+    #label: effective attack (1) or ineffective attack (0), T1, T2, score or not
+    #index: corresponding to the file at root\nba_attack2\nba_datalength.csv
+    
+    # enter your working directory under path:
+    path = '/Users/rorybunker/dataset_as_a_file_600_games.pkl'
+    
+    f = open(path, 'rb')
+    data = pickle.load(f)
+    
+    indices = array(data[2].astype(int), dtype=object)
+    trajectories = array(data[0], dtype=object)
+    label = array(data[1], dtype=object)
+    
+    label_df = pd.DataFrame(label, index=indices)
+    # or if you want to select specific teams e.g. Golden state warriors
+    nba_team_id = 1610612744
+    label_df = label_df[(label_df[6]==nba_team_id)] 
+    # and or fixed number of plays e.g. for testing purposes
+    number_of_plays = 100
+    label_df = label_df.iloc[0:number_of_plays]
+    
+    #label data is in the format [label_i,t1,t2,score,shooterID,passerID,team_ID]
+    effective = label_df.iloc[:,0]
+    scored = label_df.iloc[:,3]
+    
+    time_interval = 't2'
+    if time_interval == 't1':
+        t_interval = label_df.iloc[:,1]
+    elif time_interval == 't2':
+        t2 = label_df.iloc[:,2]
+        t_interval = label_df.iloc[:,2][t2>0]
+    
+    agent_df_list = create_df(trajectories, t_interval)
+    
+    create_point_csv(agent_df_list, effective)
+    create_trajectory_csv(agent_df_list, effective)
+    
+    # delete the table rows that were in the postgres database tables previously
+    delete_table_rows('point_ma')
+    delete_table_rows('trajectory_ma')
+    delete_table_rows('candidates_ma') 
+    
+    # import the newly created csv files into postgres database
+    import_point_ma_table_into_postgres('point_ma', path)
+    import_traj_ma_table_into_postgres('trajectory_ma', path)
+    
+if __name__ == '__main__':
+    main()
