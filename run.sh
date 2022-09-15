9985c0f131985280f71f945c91fd5df0c1cdcd97
Rory Bunker <rorybunker@gmail.com>
2022-09-15 14:12:50 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # load virtual environment
 source ${HOME}/workspace2/virtualenvs/venv/bin/activate geoenv
-# execute python scripts sequentially
-python -u /home/b_rory/workspace2/work/ma_stat_dsm/create_postgresql_db.py
-python -u /home/b_rory/workspace2/work/ma_stat_dsm/data_preprocess.py -p 4 -t 1610612744 -a shooter lastpasser
-python -u /home/b_rory/workspace2/work/ma_stat_dsm/ma_stat_dsm.py -l 5 -d 1.5
-
-# delta=$(python -u /home/b_rory/workspace2/work/ma_stat_dsm/ma_stat_dsm.py -l 5 -d 1.5)
-
-# python -u /home/b_rory/workspace2/work/ma_stat_dsm/calculate_sig_subtraj.py -d delta
+# execute python script
+python -u /home/b_rory/workspace2/work/ma_stat_dsm/main.py
