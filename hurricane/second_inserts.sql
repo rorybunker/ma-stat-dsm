eda1cd413a3f5c4231df1c6915fd26204714d5bf
Rory Bunker <rorybunker@gmail.com>
2022-10-18 13:26:29 +0900

    Update second_inserts.sql

--- hurricane/second_inserts.sql
+++ hurricane/second_inserts.sql
@@ -1,4 +1,6 @@
 INSERT INTO hurricane_point_ma(id,aid,tid,pid,label,geom) (
   SELECT id+7316, 1, tid, pid, label, geom FROM hurricane_point);
-  
+ 
+INSERT INTO hurricane_trajectory_ma(id,aid,tid,label,geom) (
+  SELECT id+210, 1, tid, label, geom FROM hurricane_trajectory);
  
