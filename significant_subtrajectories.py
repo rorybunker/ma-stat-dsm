085a56b7128ab569a8fa66be25bbe544eeea2d69
Rory Bunker <rorybunker@gmail.com>
2023-02-10 11:52:47 +0900

    Update significant_subtrajectories.py

--- significant_subtrajectories.py
+++ significant_subtrajectories.py
@@ -13,22 +13,24 @@ import sys
 from scipy.special import comb
 sys.setrecursionlimit(10000)
 from sqlalchemy import create_engine
-# import data_preprocess as prep
 import argparse
 import time
 import os
-# import warnings
-# from shapely.errors import ShapelyDeprecationWarning
-# warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
-
-# sys.path.append('court_class.py')
-
+import sys
 import court_class
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--delta', type=float, required=True, dest='delta_s')
+parser.add_argument('-d', '--delta', type=float, required=True)
 args, _ = parser.parse_known_args()
-delta_star = args.delta_s
+
+# Read the arguments from the file
+with open('args.txt', 'r') as f:
+    team = f.readline().strip()
+    game_id = f.readline().strip()
+
+with open('args_msdsm.txt', 'r') as f:
+    min_l = int(f.readline().strip())
+    dist_threshold = float(f.readline().strip())
 
 engine = create_engine('postgresql://postgres:1234@localhost:5432/postgres')
 
@@ -38,8 +40,6 @@ except:
     print("I am unable to connect to the database")
 
 cur = conn.cursor()
-
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 def count_label_number(trajectory_table, label):
     sql = """select count(distinct tid) from """ + str(trajectory_table) + """ where label = '""" + str(label) + """'"""
@@ -100,7 +100,7 @@ def get_label_of_neighb_id(trajectory_table, eps_id):
     rows = cur.fetchall()
     return rows[0][0]
 
-# functions to calculate p-values - same as stat-dsm functions except do not use current_min_p
+# functions to calculate p-values - the next three functions are the same as the (ma)-stat-dsm functions excluding current_min_p
 def calculate_upper_p_value(positive_support, negative_support, positive_number, negative_number):
 
     p_value = 0
@@ -141,6 +141,7 @@ def calculate_lower_p_value(positive_support, negative_support, positive_number,
 
     return p_value
 
+
 def calculate_p_value(positive_support, negative_support, positive_number, negative_number):
 
     upper_p = calculate_upper_p_value(positive_support, negative_support, positive_number, negative_number)
@@ -150,35 +151,21 @@ def calculate_p_value(positive_support, negative_support, positive_number, negat
 
     return p_value
 
-# create the result tables of discriminative subtrajectories and discriminative points in the postgres db
-def delete_table(table_name):
-    sql = """DROP TABLE IF EXISTS """ + table_name +""";"""
+def create_discriminative_point_table(point_table, disc_subtraj_table, team, game_id, min_l, dist_threshold):
+    dist_threshold_str = str(dist_threshold).replace(".", "_")
+    drop_table_sql = "DROP TABLE IF EXISTS disc_pts_""" + str(team) + """_""" + str(game_id) + """_""" + str(min_l) + """_""" + dist_threshold_str
+    cur.execute(drop_table_sql)
+    conn.commit()
+    sql = """CREATE TABLE disc_pts_""" + str(team) + """_""" + str(game_id) + """_""" + str(min_l) + """_""" + dist_threshold_str + """  AS SELECT p.* FROM """ + point_table + """ p INNER JOIN """ + disc_subtraj_table + """ d ON p.tid=d.tid WHERE p.pid BETWEEN d.start_idx AND d.end_idx;"""
     cur.execute(sql)
     conn.commit()
 
-def change_column_types():
-    sql = "ALTER TABLE discriminative_subtraj ALTER COLUMN tid TYPE INTEGER USING (discriminative_subtraj.tid::integer), ALTER COLUMN start_idx TYPE INTEGER USING (discriminative_subtraj.start_idx::integer), ALTER COLUMN end_idx TYPE INTEGER USING (discriminative_subtraj.end_idx::integer);"
-    cur.execute(sql)
+def create_full_traj_table(point_table_name, subtraj_table_name, team, game_id, min_l, dist_threshold):
+    dist_threshold_str = str(dist_threshold).replace(".", "_")
+    drop_table_sql = "DROP TABLE IF EXISTS points_""" + str(team) + '_' + str(game_id) + '_' + str(min_l) + '_' + dist_threshold_str
+    cur.execute(drop_table_sql)
     conn.commit()
-
-def delete_rows_discriminative_subtraj_table(disc_subtraj_table):
-    sql = """DELETE FROM """ + disc_subtraj_table + """;"""
-    cur.execute(sql)
-    conn.commit()
-
-def create_discriminative_point_table(point_table, ts):
-    sql = """CREATE TABLE discriminative_points_""" + ts + """  AS SELECT p.* FROM """ + point_table + """ p INNER JOIN discriminative_subtraj d ON p.tid=d.tid WHERE p.pid BETWEEN d.start_idx AND d.end_idx;"""
-    cur.execute(sql)
-    conn.commit()
-
-def create_discriminative_subtraj_vis_table(disc_point_table, ts):
-    sql = """CREATE TABLE discriminative_sub_traj_vis_""" + ts + """  AS SELECT aid, tid, label, ST_MakeLine(dp.geom ORDER BY pid) AS geom FROM """ + disc_point_table + """_""" + ts + """  AS dp
-		GROUP BY aid, tid, label"""
-    cur.execute(sql)
-    conn.commit()
-
-def create_full_traj_table(point_table_name, subtraj_table_name, ts):
-    table_name = "points_" + str(ts)
+    table_name = "points_" + str(team) + '_' + str(game_id) + '_' + str(min_l) + '_' + dist_threshold_str
     sql = """CREATE TABLE """ + table_name + """ AS SELECT p.* FROM """ + point_table_name + """ p INNER JOIN  """ + subtraj_table_name + """ d on ((p.aid = d.aid)) and ((p.tid = d.tid));"""
     cur.execute(sql)
     conn.commit()
@@ -188,7 +175,7 @@ def drop_all_discriminative_tables(engine):
     from sqlalchemy.sql import text
     sql =   '''SELECT tablename FROM pg_catalog.pg_tables 
         WHERE schemaname='public'
-        AND tablename LIKE 'discriminative_%%';'''
+        AND tablename LIKE 'disc_pts_%%';'''
     temp_tables = pd.read_sql(sql, engine)['tablename']
     
     with engine.connect() as con:
@@ -200,7 +187,6 @@ def drop_all_discriminative_tables(engine):
 
 def main():
     disc_subtraj_table = 'discriminative_subtraj'
-    disc_point_table = 'discriminative_points'
     candidate_table = 'candidates'
     positive_label = '1'
     negative_label = '0'
@@ -243,7 +229,6 @@ def main():
     eps_neighb_labels_df['label_0_count'] = eps_neighb_labels_df.apply(lambda s : s.value_counts().get(key='0',default='0'), axis=1)
     eps_neighb_labels_df['label_1_count'] = eps_neighb_labels_df.apply(lambda s : s.value_counts().get(key='1',default='1'), axis=1)
 
-
     # calculate the p-value for each candidate based on the label counts and total # of instances under each label
     eps_neighb_labels_df['pvalue'] = eps_neighb_labels_df.apply(lambda row:
                                                                 calculate_p_value(int(row['label_1_count']),
@@ -256,7 +241,7 @@ def main():
     final_df = pd.merge(candidate_df, eps_neighb_labels_df['pvalue'], left_index=True, right_index=True)
 
     # then, filter to only include those with p-values less than the delta* value calculated from stat-dsm
-    result_df = final_df.loc[final_df['pvalue'] < delta_star]
+    result_df = final_df.loc[final_df['pvalue'] < args.delta]
 
     if result_df.empty == True:
         print("The lowest p-value for any sub-trajectory is " + str(min(eps_neighb_labels_df['pvalue'])) + ". There are no significant sub-trajectories that have p-values below delta *")
@@ -270,31 +255,23 @@ def main():
 
         result_df.to_sql(disc_subtraj_table, engine, if_exists='replace')
 
-        change_column_types()
-
-        # get current timestamp to append to database table names
-        ts = time.gmtime()
-        ts_value = time.strftime("%s", ts)
-
-        # delete_table(disc_point_table)
-        create_discriminative_point_table(point_table, ts_value)
+        sql = "ALTER TABLE discriminative_subtraj ALTER COLUMN tid TYPE INTEGER USING (discriminative_subtraj.tid::integer), ALTER COLUMN start_idx TYPE INTEGER USING (discriminative_subtraj.start_idx::integer), ALTER COLUMN end_idx TYPE INTEGER USING (discriminative_subtraj.end_idx::integer);"
+        cur.execute(sql)
+        conn.commit()
         
-        # create table to visualize the discriminative subtrajectories
-        # delete_table('discriminative_sub_traj_vis')
-        # create_discriminative_subtraj_vis_table(disc_point_table, ts_value)
+        # delete_table(disc_point_table)
+        create_discriminative_point_table(point_table, disc_subtraj_table, team, game_id, min_l, dist_threshold)
         
         # plot the discriminative subtrajectories
         court_path = 'nba_court_T.png'
-        table_name = 'discriminative_points_' + str(ts_value)
+
+        table_name = 'disc_pts_' + str(team) + '_' + str(game_id) + '_' + str(min_l) + '_' + str(dist_threshold).replace(".", "_")
 
         # create the full trajectory table to underlay the full trajectories under the discriminative subtrajectories
-        create_full_traj_table(point_table, table_name, ts_value)
+        create_full_traj_table(point_table, table_name, team, game_id, min_l, dist_threshold)
 
+        # create the final plot(s) in the figs/ folder
         court = court_class.Court(table_name, court_path, engine)
-        
-        # if num_agents == 1:
-        #     court.plot_single_agent_trajectories()
-        # elif num_agents > 1:
         court.plot_multi_agent_trajectories()
 
         conn.close()
